e0bb4a2e98b2a31a54bcbcb54914c1ff475253c3
Kento Niwase <kniwase@landscape.co.jp>
2025-03-11 14:27:40 +0900

    fix: ローカルのDBとS3のcomposeファイルを分離

--- launch-local-containers.sh
+++ launch-local-containers.sh
@@ -5,8 +5,8 @@ set -e
 function main() {
     cd "$(dirname "$(readlink -f "$0")")/docker"
     ./generate_docker_compose
-    docker compose up --build --force-recreate
-    docker compose down
+    docker compose -f ./docker-compose.yaml -f ./docker-compose-local.yaml up --build --force-recreate
+    docker compose -f ./docker-compose.yaml -f ./docker-compose-local.yaml down
 }
 
 main $@
